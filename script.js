c7d6a79eaac6a2e02530e7b96f28e61f44afce83
Hari <hariar2.0@outlook.com>
2020-12-07 12:44:16 -0700

    Refactor the Javascript functions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const conformPassword = document.getElementById("password2");
+const confirmPassword = document.getElementById("password2");
 
 // Show Inout Error Messages
 function showError(input, message) {
@@ -19,40 +19,31 @@ function showSuccess(input) {
   //   smallText.innerText = "";
 }
 
+// Check Required Fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    console.log(input.value);
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
 // Email Validation
 function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log("Submit" + username.value);
-
-  if (username.value === "") {
-    showError(username, "Username is Required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === "") {
-    showError(email, "Email is Required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Email is in NOT correct format");
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === "") {
-    showError(password, "Password is Required");
-  } else {
-    showSuccess(password);
-  }
-
-  if (conformPassword.value === "") {
-    showError(conformPassword, "Confirm Password is Required");
-  } else {
-    showSuccess(conformPassword);
-  }
+  checkRequired([username, email, password, confirmPassword]);
 });
